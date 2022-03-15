1d577267f83524f119710a40cd24bc25b4cb1e9c
bingo1227 <johnhelard@gmail.com>
2022-03-14 19:00:23 -0700

    final commit

---
+++ contracts/photo-nft-data/commons/PhotoNFTDataStorages.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { PhotoNFTDataObjects } from "./PhotoNFTDataObjects.sol";
+
+
+// shared storage
+contract PhotoNFTDataStorages is PhotoNFTDataObjects {
+
+    Photo[] public photos;
+
+}
+
